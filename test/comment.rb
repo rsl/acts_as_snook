874c63b1de76b26371a32ca814e310bf5de2a781
rsl <rsl@luckysneaks.com>
2008-09-24 08:32:44 -0400

    modified data setup for association code

--- test/comment.rb
+++ test/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :entry
+  
   acts_as_snook # Defaults
 end
--- test/schema.rb
+++ test/schema.rb
@@ -2,7 +2,13 @@ ActiveRecord::Migration.verbose = false
 
 ActiveRecord::Schema.define(:version => 1) do
   create_table :comments, :force => true do |t|
+    t.integer :entry_id
     t.string :author, :email, :url, :spam_status
     t.text :body
+  end
+  
+  create_table :entries, :force => true do |t|
+    t.string :title
+    t.integer :ham_comments_count, :default => 0
   end
 end
